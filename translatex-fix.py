702e4723b276b37a652ed56fb98db5ab6ddff2eb
Quentin Barbarat <quentin.barbarat@epita.fr>
2019-07-27 13:57:59 +0200

    Remove hardcoded values

--- translatex-fix.py
+++ translatex-fix.py
@@ -1,16 +1,31 @@
-in_file = open('input.txt','r')
+#!/usr/bin/env python3
 
-linenum = 1
+import sys
 
-outtext_file = open("0-txt.txt", 'w')
-outbal_file = open("0-bal.txt", "w")
-for line in in_file:
-    if linenum%100 == 0:
-        outtext_file = open(str(linenum // 100)+"-txt.txt",'w')
-        outbal_file = open(str(linenum // 100)+"-bal.txt",'w')
-    if line[0] == "\\":
-        outbal_file.write(line)
-    else:
-        outbal_file.write("line" + str(linenum)+"\n")
-        outtext_file.write(line)
-    linenum += 1
+def main():
+
+    if len(sys.argv) < 2:
+        print("Please pass your filename as an argument")
+        return 1
+
+    in_file = open(sys.argv[1],'r')
+
+    linenum = 1
+
+    outtext_file = open("0-txt.txt", 'w')
+    outbal_file = open("0-bal.txt", "w")
+    for line in in_file:
+        if linenum%100 == 0:
+            outtext_file = open(str(linenum // 100)+"-txt.txt",'w')
+            outbal_file = open(str(linenum // 100)+"-bal.txt",'w')
+        if line[0] == "\\":
+            outbal_file.write(line)
+        else:
+            outbal_file.write("line" + str(linenum)+"\n")
+            outtext_file.write(line)
+        linenum += 1
+
+    return 0
+
+if __name__ == "__main__":
+    main()
--- translatex-reassemble.py
+++ translatex-reassemble.py
@@ -1,10 +1,24 @@
-out_file = open("out.txt",'w')
+#!/usr/bin/env python3
 
-for x in range(0,7):
-    inbal_file = open(str(x)+"-bal.txt",'r')
-    intxt_file = open(str(x)+"-en.txt",'r')
-    for line in inbal_file:
-        if line[0] == "l":
-            out_file.write(intxt_file.readline())
-        else:
-            out_file.write(line)
+import sys
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Please pass the max number as an argument")
+        return 1
+
+    out_file = open("out.txt",'w')
+
+    for x in range(0,int(sys.argv[1])):
+        inbal_file = open(str(x)+"-bal.txt",'r')
+        intxt_file = open(str(x)+"-en.txt",'r')
+        for line in inbal_file:
+            if line[0] == "l":
+                out_file.write(intxt_file.readline())
+            else:
+                out_file.write(line)
+    return 0
+
+if __name__ == "__main__":
+    main()
